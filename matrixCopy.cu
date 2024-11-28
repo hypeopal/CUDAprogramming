5fddfb1cf8c37c6cc7cfc18af7a1d5c066b079f5
hypeopal <136549095+hypeopal@users.noreply.github.com>
2024-11-28 12:24:24 +0800

    Update matrixCopy.cu
    change the number of element copied by each thread

--- matrixCopy.cu
+++ matrixCopy.cu
@@ -11,53 +11,55 @@ const int testTimes = 10;
 __constant__ int devMatrixSize;
 
 
-__global__ void matrixCopyInRow(int* newMatrix, const int* oldMatrix) { //行优先
-	int x = blockIdx.x * gridDim.x + threadIdx.x * 4;
+__global__ void matrixCopyInRow(int* newMatrix, const int* oldMatrix, int numPerThread) { //行优先
+	int x = blockIdx.x * gridDim.x + threadIdx.x * numPerThread;
 	int y = blockIdx.y * gridDim.y + threadIdx.y;
 	int i = 0;
-	while (i < 4) {
+	while (i < numPerThread) {
 		newMatrix[y * devMatrixSize + x + i] = oldMatrix[y * devMatrixSize + x + i];
 		++i;
 	}
 }
 
-__global__ void matrixCopyInCol(int* newMatrix, const int* oldMatrix) { //列优先
+__global__ void matrixCopyInCol(int* newMatrix, const int* oldMatrix, int numPerThread) { //列优先
 	int x = blockIdx.x * gridDim.x + threadIdx.x;
-	int y = blockIdx.y * gridDim.y + threadIdx.y * 4;
+	int y = blockIdx.y * gridDim.y + threadIdx.y * numPerThread;
 	int i = 0;
-	while (i < 4) {
+	while (i < numPerThread) {
 		newMatrix[(y + i) * devMatrixSize + x] = oldMatrix[(y + i) * devMatrixSize + x];
 		++i;
 	}
 }
 
-cudaError_t cuda_check(cudaError_t error_code, int line)
+cudaError_t cuda_check(const cudaError_t& error_code, int line)
 {
 	if (error_code != cudaSuccess)
 	{
 		printf("line: %d, error_code: %d, error_name: %s, error_description: %s\n",
 			line, error_code, cudaGetErrorName(error_code), cudaGetErrorString(error_code));
-		exit(EXIT_FAILURE); // 检测到CUDA错误时退出程序  
+		exit(EXIT_FAILURE); // 检测到CUDA错误时退出程序
 	}
 	return error_code;
 }
 
-void runMatrixCopy(const int* matrix, int* copiedMatrix, const std::string& type) {
+void runMatrixCopy(const int* matrix, int* copiedMatrix, int numPerThread, const std::string& type) {
 	std::vector<float> runtimeList; //运行时间
 	int size = sizeof(int) * matrixSize * matrixSize;
 	int* cudaOldMatrix;
+	//__device__ int devNumPerThread;
+	//cuda_check(cudaMemcpyToSymbol(devNumPerThread, &numPerThread, sizeof(int)), __LINE__);
 	cuda_check(cudaMalloc((void**)&cudaOldMatrix, size), __LINE__);
 	cuda_check(cudaMemcpy(cudaOldMatrix, matrix, size, cudaMemcpyHostToDevice), __LINE__);
 	for (int n = 0; n < testTimes; ++n) {
 		int* cudaNewMatrix;
 		cuda_check(cudaMalloc((void**)&cudaNewMatrix, size), __LINE__);
-		
+
 		dim3 dimGrid(tileSize, tileSize, 1);
 		dim3 dimBlock;
-		if (type == "Row") 
-			dimBlock = dim3(threadSize, tileSize, 1);
-		else 
-			dimBlock = dim3(tileSize, threadSize, 1);
+		if (type == "Row")
+			dimBlock = dim3(tileSize / numPerThread, tileSize, 1);
+		else
+			dimBlock = dim3(tileSize, tileSize / numPerThread, 1);
 
 		// 记录GPU时间
 		cudaEvent_t start, stop;
@@ -66,10 +68,10 @@ void runMatrixCopy(const int* matrix, int* copiedMatrix, const std::string& type
 
 		//开始运行
 		cuda_check(cudaEventRecord(start, 0), __LINE__);
-		if (type == "Row") 
-			matrixCopyInRow <<<dimGrid, dimBlock >>> (cudaNewMatrix, cudaOldMatrix);
-		else 
-			matrixCopyInCol <<<dimGrid, dimBlock >>> (cudaNewMatrix, cudaOldMatrix);
+		if (type == "Row")
+			matrixCopyInRow << <dimGrid, dimBlock >> > (cudaNewMatrix, cudaOldMatrix, numPerThread);
+		else
+			matrixCopyInCol << <dimGrid, dimBlock >> > (cudaNewMatrix, cudaOldMatrix, numPerThread);
 		cuda_check(cudaDeviceSynchronize(), __LINE__);
 		cuda_check(cudaEventRecord(stop, 0), __LINE__);
 
@@ -80,6 +82,7 @@ void runMatrixCopy(const int* matrix, int* copiedMatrix, const std::string& type
 
 		cuda_check(cudaMemcpy(copiedMatrix, cudaNewMatrix, size, cudaMemcpyDeviceToHost), __LINE__);
 		cuda_check(cudaFree(cudaNewMatrix), __LINE__);
+		//_ASSERT(cudaGetLastError() == cudaSuccess);
 
 		//结果检查
 		bool correct = true;
@@ -95,8 +98,7 @@ void runMatrixCopy(const int* matrix, int* copiedMatrix, const std::string& type
 		}
 	}
 	cuda_check(cudaFree(cudaOldMatrix), __LINE__);
-	std::cout << type << std::endl;
-	std::cout << "Result:" << "correct" << std::endl;
+	std::cout << "Result in " << type << " with " << numPerThread <<" nums per thread:correct" << std::endl;
 	//计算平均时间
 	std::cout << "Average runtime: " << std::accumulate(runtimeList.begin(), runtimeList.end(), 0.0f) / testTimes << std::endl;
 }
@@ -109,10 +111,16 @@ int main() {
 		matrix[i] = i;
 	}
 	cuda_check(cudaMemcpyToSymbol(devMatrixSize, &matrixSize, sizeof(int)), __LINE__);
-	
-	runMatrixCopy(matrix, copiedMatrix, "Row");
+
+	int numList[]{1, 2, 4, 8, 16, 32};
+
+	for (auto i : numList) {
+		runMatrixCopy(matrix, copiedMatrix, i, "Row");
+	}
 	std::cout << std::endl;
-	runMatrixCopy(matrix, copiedMatrix, "Col");
+	for (auto i : numList) {
+		runMatrixCopy(matrix, copiedMatrix, i, "Col");
+	}
 
 	delete[] matrix;
 	delete[] copiedMatrix;
